f859ee5e3236046f39f6c1124ff92f0d216b74b3
alerafart <maalejandrarafart@gmail.com>
2022-12-22 10:21:02 +0100

    first commit

---
+++ src/components/Result.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="result">
+        <div class="title">{{ results[resultIndex].title }}</div>
+        <div class="desc">
+            {{ results[resultIndex].desc }}
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: ["results", "totalCorrect"],
+    computed: {
+        resultIndex() {
+            let index = 0;
+
+            // checks if total correct is above the min and below the max
+            // it only executes if the results or totalcorrect properties  update
+            // if a user enters an incorrect answer this function wont run again
+            // saving ressources
+            this.results.forEach((e, i) =>  {
+                if(e.min <= this.totalCorrect && e.max >= this.totalCorrect) {
+                    index = i;
+                }
+            })
+            return index;
+        }
+    }
+};
+
+</script>
